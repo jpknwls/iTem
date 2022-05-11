8cdb6d4a9bc3b20600bfaaeb89a5e255e499964b
jpknwls <28238972+jpknwls@users.noreply.github.com>
2022-05-10 21:22:44 -0700

    initial xcode project

---
+++ iTem/Shared/iTemApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTemApp.swift
+//  Shared
+//
+//  Created by Dana Knowles on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iTemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
